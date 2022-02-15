c70ac33475870de130a8f4a778d2b97f4d7fa28f
tansen <tansenkhan1990@gmail.com>
2022-02-15 20:40:40 +0100

    add various style

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -2,11 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 const Container=styled.div`
 height:60px;
-background-color:black;
 `
+const Wrapper = styled.div`
+padding : 10px 20px;
+display : flex;
+justify-content : space-between;
+`
+const Left = styled.div`
+flex : 1;
+`
+const Language = styled.div`
+font-size: 30px;
+cursor:pointer;
+`
+const Center = styled.div`flex : 1;`
+const Right = styled.div`flex : 1;`
 function Navbar() {
   return (
-   <Container>Navbar</Container>
+   <Container>
+       <Wrapper>
+           <Left>pokemon left <Language>EN</Language></Left>
+           <Center>Center</Center>
+           <Right>right</Right>
+       </Wrapper>
+   </Container>
   )
 }
 
