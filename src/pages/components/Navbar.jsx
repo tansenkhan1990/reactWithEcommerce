b096716aa04acc44dcf54511e8b59fc153863e18
tansen <tansenkhan1990@gmail.com>
2022-02-15 22:12:04 +0100

    complete navbar design

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
+import {Badge} from '@material-ui/core' 
+
 const Container=styled.div`
 height:60px;
 `
@@ -7,23 +10,54 @@ const Wrapper = styled.div`
 padding : 10px 20px;
 display : flex;
 justify-content : space-between;
+align-items : center;
 `
 const Left = styled.div`
 flex : 1;
+display : flex;
+align-items : center;
 `
 const Language = styled.div`
 font-size: 30px;
 cursor:pointer;
 `
-const Center = styled.div`flex : 1;`
-const Right = styled.div`flex : 1;`
+const SearchContainer = styled.div`
+border: .5px solid lightgray;
+display : flex;
+align-items : center;
+margin-left : 25px;
+padding : 5px;
+` 
+const Input = styled.input`border:none;`
+const Center = styled.div`flex : 1; text-align:center;`
+const Right = styled.div`
+flex : 1; 
+display: flex; 
+text-align: center;
+justify-content: flex-end;
+`
+const MenuItem = styled.div`
+font-size : 14px; 
+cursor:pointer;
+margin-left: 25px;
+`
 function Navbar() {
   return (
    <Container>
        <Wrapper>
-           <Left>pokemon left <Language>EN</Language></Left>
+           <Left><Language>EN</Language>
+           <SearchContainer><Input /><Search style={{color:'gray', fontSize:16}}/></SearchContainer>
+           </Left>
            <Center>Center</Center>
-           <Right>right</Right>
+           <Right>
+               <MenuItem>Register</MenuItem>
+               <MenuItem>SignIn</MenuItem>
+               <MenuItem>
+               <Badge badgeContent={4} color="primary">
+                   <ShoppingCartOutlined />
+                   </Badge>
+                   </MenuItem>
+           </Right>
        </Wrapper>
    </Container>
   )
